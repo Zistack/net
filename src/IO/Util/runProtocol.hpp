c8f0a61632c79c93d242a178016090227b36b3b5
Zistack <Zistack@null.net>
2018-09-01 15:24:26 -0700

    Added abstract protocol event loop

---
+++ src/IO/Util/runProtocol.hpp
@@ -0,0 +1,54 @@
+void
+runProtocol (Interface::Protocol::T * protocol,
+    Failure::ExceptionStore::T & exception_store,
+    Interface::NonblockingInputStream::T * input_stream,
+    Interface::NonblockingOutputStream::T * output_stream,
+    Signal::T * shutdown_signal)
+{
+	try
+	{
+		protocol->init (input_stream, output_stream);
+	}
+	catch (const Failure::Throwable::T & e)
+	{
+		exception_store.store (e);
+		return;
+	}
+
+	try
+	{
+		while (true)
+		{
+			try
+			{
+				IO::Util::wait (input_stream, shutdown_signal);
+			}
+			catch (const Failure::CancelError::T & e)
+			{
+				break;
+			}
+
+			try
+			{
+				protocol->event ();
+			}
+			catch (const IO::EOF::T & e)
+			{
+				break;
+			}
+		}
+	}
+	catch (const Failure::Throwable::T & e)
+	{
+		exception_store.store (e);
+	}
+
+	try
+	{
+		protocol->clean ();
+	}
+	catch (const Failure::Throwable::T & e)
+	{
+		exception_store.store (e);
+	}
+}
