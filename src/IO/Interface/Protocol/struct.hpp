bdd01bbbce49c3d34a522414c9ec0cb4de450f53
Zistack <Zistack@null.net>
2018-09-29 17:54:18 -0700

    Changed abstract protocol interface

--- src/IO/Interface/Protocol/struct.hpp
+++ src/IO/Interface/Protocol/struct.hpp
@@ -1,16 +1,11 @@
 struct T
 {
 	virtual void
-	init (IO::Interface::NonblockingInputStream::T * input_stream,
-	    IO::Interface::NonblockingOutputStream::T * output_stream,
-	    Failure::ExceptionStore::T & exception_store,
-	    std::function<void(void)> stop) = 0;
+	run (IO::Interface::NonblockingInputStream::T * input_stream,
+	    IO::Interface::NonblockingOutputStream::T * output_stream) = 0;
 
 	virtual void
-	event () = 0;
-
-	virtual void
-	clean () = 0;
+	stop () = 0;
 
 	virtual ~T () = default;
 };
