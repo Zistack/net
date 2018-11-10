cab164bacf419427bc5ecab4c9ac9f35c4c38026
Zistack <Zistack@null.net>
2018-11-10 11:49:07 -0800

    Fixed Protocol to use references instead of pointers

--- src/Protocol/eventLoop.hpp
+++ src/Protocol/eventLoop.hpp
@@ -1,17 +1,15 @@
 void
 eventLoop (Failure::ExceptionStore::T & exception_store,
-    IO::Interface::Watchable::T * watchable,
-    Shutdown::Signal::T & shutdown_signal,
+    IO::Interface::Watchable::T & watchable,
+    IO::Interface::Watchable::T & shutdown_signal,
     std::function<void(void)> event)
 {
-	Shutdown::Scope::T shutdown_scope (shutdown_signal, &exception_store);
-
 	exception_store.tryStore ([&]() {
 		while (true)
 		{
 			try
 			{
-				IO::Util::wait (watchable, &shutdown_signal);
+				IO::Util::wait (watchable, shutdown_signal);
 			}
 			catch (Failure::CancelException::T)
 			{
