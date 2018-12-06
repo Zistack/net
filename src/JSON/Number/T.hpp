74cf05fbee9b52be74450077f6831b6376fb798a
Zistack <Zistack@null.net>
2018-12-06 15:49:24 -0800

    Updated JSON to use new IO::String interface

--- src/JSON/Number/T.hpp
+++ src/JSON/Number/T.hpp
@@ -5,13 +5,16 @@ T::T (IO::Interface::PeekableInputStream::T & input_stream)
 
 T::T (std::string number)
 {
-	IO::String::T string_stream (number);
+	IO::String::T string (number);
 
-	init (string_stream);
+	IO::String::PeekableInputStream::T & input_stream =
+	    string.peekableInputStream ();
 
-	if (!string_stream.eof ())
+	init (input_stream);
+
+	if (!input_stream.eof ())
 	{
 		throw Failure::Error::T ("JSON::Number::T\n" +
-		    IO::Message::unexpectedCharacter (string_stream.peek ()));
+		    IO::Message::unexpectedCharacter (input_stream.peek ()));
 	}
 }
