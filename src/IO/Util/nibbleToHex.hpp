2be71a4b3f33b0e3baaa3ce13d97b3154c3ffce9
Zistack <Zistack@null.net>
2018-12-15 19:20:19 -0800

    Added option for hex conversion to produce lowercase output

--- src/IO/Util/nibbleToHex.hpp
+++ src/IO/Util/nibbleToHex.hpp
@@ -1,8 +1,17 @@
+template <Case::T c
+#ifndef IO_UTIL_NIBBLE_TO_HEX
+#define IO_UTIL_NIBBLE_TO_HEX
+    = Case::UPPER
+#endif /* IO_UTIL_NIBBLE_TO_HEX */
+    >
 char
 nibbleToHex (char n)
 {
 	if (n < 10) return (char) ('0' + n);
-	if ((n >= 10) && (n < 16)) return (char) ('A' + n);
+	if ((n >= 10) && (n < 16))
+	{
+		return (char) ((c == Case::UPPER ? 'A' : 'a') + n);
+	}
 
 	return 0; // Unreachable.
 }
