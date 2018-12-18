cd26f24520a727b1022ed0a591b818d36dc358e4
Zistack <Zistack@null.net>
2018-12-17 17:05:22 -0800

    Fixed consume utility breaking when passed a void-returning parsing rule

--- src/IO/Util/consume.hpp
+++ src/IO/Util/consume.hpp
@@ -9,9 +9,15 @@ consume (const std::string & input_string, Rule && rule)
 	return consume (input_stream, std::forward<Rule> (rule));
 }
 
-template <class Rule>
-decltype (std::declval<Rule> () (
-    std::declval<IO::Interface::PeekableInputStream::T &> ()))
+template <class Rule,
+    typename
+#ifndef IO_Util_consume_hpp_2
+#define IO_Util_consume_hpp_2
+    = typename std::enable_if<!std::is_void<typename std::invoke_result<Rule,
+        IO::Interface::PeekableInputStream::T &>::type>::value>::type
+#endif /* IO_Util_consume_hpp_2 */
+    >
+typename std::invoke_result<Rule, IO::Interface::PeekableInputStream::T &>::type
 consume (IO::Interface::PeekableInputStream::T & input_stream, Rule && rule)
 {
 	auto result = rule (input_stream);
@@ -23,4 +29,24 @@ consume (IO::Interface::PeekableInputStream::T & input_stream, Rule && rule)
 	}
 
 	return result;
+}
+
+template <class Rule,
+    typename
+#ifndef IO_Util_consume_hpp_3
+#define IO_Util_consume_hpp_3
+    = typename std::enable_if<std::is_void<typename std::invoke_result<Rule,
+        IO::Interface::PeekableInputStream::T &>::type>::value>::type
+#endif /* IO_Util_consume_hpp_3 */
+    >
+void
+consume (IO::Interface::PeekableInputStream::T & input_stream, Rule && rule)
+{
+	rule (input_stream);
+
+	if (!input_stream.eof ())
+	{
+		throw Failure::SyntaxError::T (
+		    Message::unexpectedCharacter (input_stream.peek ()));
+	}
 }
