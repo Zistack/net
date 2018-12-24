3588391560a60fb63b4841e140acbe48aaef8a64
Zistack <Zistack@null.net>
2018-12-24 14:35:46 -0800

    Fixed hex conversion to properly use the range A-F/a-f

--- src/IO/Util/nibbleToHex.hpp
+++ src/IO/Util/nibbleToHex.hpp
@@ -10,7 +10,7 @@ nibbleToHex (char n)
 	if (n < 10) return (char) ('0' + n);
 	if ((n >= 10) && (n < 16))
 	{
-		return (char) ((c == Case::UPPER ? 'A' : 'a') + n);
+		return (char) ((c == Case::UPPER ? 'A' : 'a') + (n - 10));
 	}
 
 	return 0; // Unreachable.
