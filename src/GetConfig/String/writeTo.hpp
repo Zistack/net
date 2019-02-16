9d521e3e1dfe70f0a361be38ddc8625bd8806caf
Zistack <Zistack@null.net>
2019-02-16 15:00:49 -0800

    Renamed some local variables to be more consistent

--- src/GetConfig/String/writeTo.hpp
+++ src/GetConfig/String/writeTo.hpp
@@ -1,9 +1,9 @@
 void
-T::writeTo (const Member::Value::T & member,
+T::writeTo (const Member::Value::T & value,
     IO::Interface::OutputStream::T & output_stream,
     size_t indentation) const
 {
-	const ValueType & string = dynamic_cast<const ValueType &> (member);
+	const ValueType & string = dynamic_cast<const ValueType &> (value);
 
 	string.writeTo (output_stream, indentation);
 }
