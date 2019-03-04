3716829c9fe97dbda6c149f32bf35e2e6de72e31
Zistack <Zistack@null.net>
2019-03-03 21:47:04 -0800

    Changed IO::Util::indent to use templates

--- src/IO/Util/indent.hpp
+++ src/IO/Util/indent.hpp
@@ -1,5 +1,6 @@
+template <typename OutputStream>
 void
-indent (IO::Interface::OutputStream::T & output_stream, size_t indentation)
+indent (OutputStream && output_stream, size_t indentation)
 {
 	for (; indentation > 0; --indentation) output_stream.put ('\t');
 }
