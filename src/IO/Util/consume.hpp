ba96d715a414db964c76df82eb4ae9007280ddae
Zistack <Zistack@null.net>
2019-03-03 21:31:28 -0800

    Changed IO::Util::consume to use more templates and new stream types

--- src/IO/Util/consume.hpp
+++ src/IO/Util/consume.hpp
@@ -1,26 +1,25 @@
-template <class Rule>
-decltype (std::declval<Rule> () (
-    std::declval<Interface::PeekableInputStream::T &> ()))
-consume (const std::string & input_string, Rule && rule)
+template <typename Rule>
+typename std::invoke_result<Rule, String::Reader::T>::type
+consume (const std::string & string, Rule && rule)
 {
-	std::string::size_type pointer = 0;
-	String::PeekableInputStream::T input_stream (input_string, pointer);
+	String::Reader::T input_stream (string);
 
 	return consume (input_stream, std::forward<Rule> (rule));
 }
 
-template <class Rule,
+template <typename InputStream,
+    typename Rule,
     typename
 #ifndef IO_Util_consume_hpp_2
 #define IO_Util_consume_hpp_2
-    = typename std::enable_if<!std::is_void<typename std::invoke_result<Rule,
-        Interface::PeekableInputStream::T &>::type>::value>::type
+    = typename std::enable_if<!std::is_void<
+        typename std::invoke_result<Rule, InputStream>::type>::value>::type
 #endif /* IO_Util_consume_hpp_2 */
     >
-typename std::invoke_result<Rule, Interface::PeekableInputStream::T &>::type
-consume (Interface::PeekableInputStream::T & input_stream, Rule && rule)
+typename std::invoke_result<Rule, InputStream>::type
+consume (InputStream && input_stream, Rule && rule)
 {
-	auto result = rule (input_stream);
+	auto result = rule (std::forward<InputStream> (input_stream));
 
 	if (!input_stream.eof ())
 	{
@@ -31,18 +30,19 @@ consume (Interface::PeekableInputStream::T & input_stream, Rule && rule)
 	return result;
 }
 
-template <class Rule,
+template <typename InputStream,
+    typename Rule,
     typename
 #ifndef IO_Util_consume_hpp_3
 #define IO_Util_consume_hpp_3
-    = typename std::enable_if<std::is_void<typename std::invoke_result<Rule,
-        Interface::PeekableInputStream::T &>::type>::value>::type
+    = typename std::enable_if<std::is_void<
+        typename std::invoke_result<Rule, InputStream>::type>::value>::type
 #endif /* IO_Util_consume_hpp_3 */
     >
 void
-consume (Interface::PeekableInputStream::T & input_stream, Rule && rule)
+consume (InputStream && input_stream, Rule && rule)
 {
-	rule (input_stream);
+	rule (std::forward<InputStream> (input_stream));
 
 	if (!input_stream.eof ())
 	{
