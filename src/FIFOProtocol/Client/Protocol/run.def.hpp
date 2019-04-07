93eb46da68d89f7b8a360996f534569f070edfa0
Zistack <Zistack@null.net>
2019-04-07 12:40:46 -0700

    Removed unnecessary scope

--- src/FIFOProtocol/Client/Protocol/run.def.hpp
+++ src/FIFOProtocol/Client/Protocol/run.def.hpp
@@ -7,23 +7,21 @@ T <Request, Response, Interface>::run
 	OutputStream && output_stream
 )
 {
-	{
-		Thread::Nursery::T nursery;
+	Thread::Nursery::T nursery;
 
-		nursery.add
-		(
-			this -> input,
-			& Input::T <Response, Interface>::template run <InputStream>,
-			& this -> input,
-			std::forward <InputStream> (input_stream)
-		);
+	nursery.add
+	(
+		this -> input,
+		& Input::T <Response, Interface>::template run <InputStream>,
+		& this -> input,
+		std::forward <InputStream> (input_stream)
+	);
 
-		nursery.run
-		(
-			this -> output,
-			& Output::T <Request, Interface>::template run <OutputStream>,
-			& this -> output,
-			std::forward <OutputStream> (output_stream)
-		);
-	}
+	nursery.run
+	(
+		this -> output,
+		& Output::T <Request, Interface>::template run <OutputStream>,
+		& this -> output,
+		std::forward <OutputStream> (output_stream)
+	);
 }
