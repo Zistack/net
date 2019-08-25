e1ad908719ee5841146518120013235769e26064
Zistack <Zistack@null.net>
2019-08-25 18:49:28 -0400

    Added missing constructor declaration and access modifiers

--- src/FIFOProtocol/Client/Protocol/Interface/struct.def.hpp
+++ src/FIFOProtocol/Client/Protocol/Interface/struct.def.hpp
@@ -3,6 +3,11 @@ struct T : Implementation::T <Request, Response, Details>
 {
 	using Implementation::T <Request, Response, Details>::T;
 
+private:
+
+	using Input::T <T, Response, Details>::input;
+	using Output::T <T, Response, Details>::output;
+
 	friend Input::T <T, Response, Details>;
 	friend Output::T <T, Request, Details>;
 };
--- src/FIFOProtocol/Server/Connection/Protocol/Interface/struct.def.hpp
+++ src/FIFOProtocol/Server/Connection/Protocol/Interface/struct.def.hpp
@@ -1,6 +1,13 @@
 template <typename Request, typename Response, typename Details>
 struct T : Implementation::T <Request, Response, Details>
 {
+	using Implementation::T <Request, Response, Details>::T;
+
+private:
+
+	using Input::T <T, Request, Response, Details>::input;
+	using Output::T <T, Response, Details>::output;
+
 	friend Input::T <T, Request, Response, Details>;
 	friend Output::T <T, Response, Details>;
 };
