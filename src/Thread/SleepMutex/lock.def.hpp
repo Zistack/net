5eacfb06b352886892e4613874695ba49180973a
Zistack <Zistack@null.net>
2019-08-26 13:50:32 -0400

    Removed useless mutex wrapper

--- src/Thread/SleepMutex/lock.def.hpp
+++
@@ -1,9 +0,0 @@
-void
-T::lock ()
-{
-	std::unique_lock lock (this -> m);
-
-	while (this -> locked) this -> c . wait (lock);
-
-	this -> locked = true;
-}
--- src/Thread/SleepMutex/unlock.def.hpp
+++
@@ -1,9 +0,0 @@
-void
-T::unlock ()
-{
-	std::unique_lock lock (this -> m);
-
-	this -> locked = false;
-
-	this -> c . notify_one ();
-}
