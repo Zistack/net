4f962653804dd63a498d7c33c18e062c9cba2760
Zistack <Zistack@null.net>
2019-09-13 17:11:21 -0400

    Added address-of operators to disambiguate reference to member function from an attempt to call the member function

--- src/FIFOProtocol/Client/Protocol/Implementation/makeRequest.def.hpp
+++ src/FIFOProtocol/Client/Protocol/Implementation/makeRequest.def.hpp
@@ -17,7 +17,7 @@ T <Request, Response, Details>::makeRequest (const Request & request)
 		Thread::Timer::T round_trip_timer
 		(
 			this -> m_round_trip_timeout,
-			Thread::Delay::T <Response>::cancel,
+			& Thread::Delay::T <Response>::cancel,
 			& response_delay
 		);
 		return response_delay . get ();
@@ -47,7 +47,7 @@ T <Request, Response, Details>::makeRequest (Request && request)
 		Thread::Timer::T round_trip_timer
 		(
 			this -> m_round_trip_timeout,
-			Thread::Delay::T <Response>::cancel,
+			& Thread::Delay::T <Response>::cancel,
 			& response_delay
 		);
 		return response_delay . get ();
