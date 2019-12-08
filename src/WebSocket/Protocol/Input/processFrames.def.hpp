57ecb8397a2695fbd85202dbdc22a6b14edcf9eb
Zistack <Zistack@null.net>
2019-12-08 16:43:44 -0500

    Fixed how method call was passed into eventLoop

--- src/WebSocket/Protocol/Input/processFrames.def.hpp
+++ src/WebSocket/Protocol/Input/processFrames.def.hpp
@@ -8,8 +8,9 @@ T <Interface>::processFrames (InputStream && input_stream)
 		this -> m_exception_store,
 		std::forward <InputStream> (input_stream),
 		this -> m_input_shutdown_signal,
-		& T::processEvent <InputStream>,
-		this,
-		std::forward <InputStream> (input_stream)
+		[&] ()
+		{
+			this -> processEvent (std::forward <InputStream> (input_stream));
+		}
 	);
 }
