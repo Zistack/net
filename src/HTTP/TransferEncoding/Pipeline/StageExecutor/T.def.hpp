8fdf23b4981040af0a963da184efdc48ac9c649b
Zistack <Zistack@null.net>
2019-12-08 17:04:53 -0500

    Made Stage const in StageExecutor

--- src/HTTP/TransferEncoding/Pipeline/StageExecutor/T.def.hpp
+++ src/HTTP/TransferEncoding/Pipeline/StageExecutor/T.def.hpp
@@ -2,11 +2,11 @@ template <typename InputStream, typename Stage, typename OutputStream>
 T <InputStream, Stage, OutputStream>::T
 (
 	InputStream input_stream,
-	Stage & stage,
+	const Stage & stage,
 	OutputStream output_stream
 )
-:	m_input_stream (input_stream),
+:	m_input_stream (std::forward <InputStream> (input_stream)),
 	m_stage (stage),
-	m_output_stream (output_stream)
+	m_output_stream (std::forward <OutputStream> (output_stream))
 {
 }
--- src/HTTP/TransferEncoding/Pipeline/StageExecutor/struct.def.hpp
+++ src/HTTP/TransferEncoding/Pipeline/StageExecutor/struct.def.hpp
@@ -1,7 +1,7 @@
 template <typename InputStream, typename Stage, typename OutputStream>
 struct T
 {
-	T (InputStream input_stream, Stage & stage, OutputStream output_stream);
+	T (InputStream input_stream, const Stage & stage, OutputStream output_stream);
 
 	void
 	run ();
@@ -14,7 +14,10 @@ struct T
 private:
 
 	InputStream m_input_stream;
-	Stage & m_stage;
+	const Stage & m_stage;
 	OutputStream m_output_stream;
 };
 
+template <typename InputStream, typename Stage, typename OutputStream>
+T (InputStream && input_stream, const Stage & stage, OutputStream && output_stream) ->
+	T <InputStream, Stage, OutputStream>;
