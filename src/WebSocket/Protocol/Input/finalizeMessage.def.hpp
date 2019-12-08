2a0df6f206e8cbf2b17f9687e6416ac9cf6f8e82
Zistack <Zistack@null.net>
2019-12-08 16:42:27 -0500

    Fix typo

--- src/WebSocket/Protocol/Input/finalizeMessage.def.hpp
+++ src/WebSocket/Protocol/Input/finalizeMessage.def.hpp
@@ -2,7 +2,7 @@ template <typename Interface>
 void
 T <Interface>::finalizeMessage ()
 {
-	this -> interface () . dispatch (std::move (* this -> message));
+	this -> interface () . dispatch (std::move (* this -> m_message));
 
 	this -> m_message = std::nullopt;
 }
