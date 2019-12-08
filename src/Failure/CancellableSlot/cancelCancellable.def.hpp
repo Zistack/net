b352f8bddc503c33e9298179695f69cbd7530e9f
Zistack <Zistack@null.net>
2019-12-08 16:57:11 -0500

    Added missing level of indirection

--- src/Failure/CancellableSlot/cancelCancellable.def.hpp
+++ src/Failure/CancellableSlot/cancelCancellable.def.hpp
@@ -4,20 +4,23 @@ T <Cancellables ...>::cancelCancellable ()
 {
 	std::visit
 	(
-		[] (auto && cancellable)
+		[] (auto && cancellable_wrapper)
 		{
-			using Cancellable = decltype (cancellable);
+			using CancellableWrapper = decltype (cancellable_wrapper);
 
 			if constexpr
 			(
 				! std::is_same_v
 				<
-					std::remove_cv_t <std::remove_reference_t <Cancellable>>,
+					std::remove_cv_t
+					<
+						std::remove_reference_t <CancellableWrapper>
+					>,
 					std::nullptr_t
 				>
 			)
 			{
-				cancellable . cancel ();
+				cancellable_wrapper . get () . cancel ();
 			}
 		},
 		this -> m_cancellable
