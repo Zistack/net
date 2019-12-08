6c186e4f3c0de4412934bc23e40aa5cd02ff8109
Zistack <Zistack@null.net>
2019-12-08 16:32:20 -0500

    Removed pointless forwarding

--- src/IO/Rule/putHex.def.hpp
+++ src/IO/Rule/putHex.def.hpp
@@ -2,5 +2,5 @@ template <typename Number, typename OutputStream, typename>
 void
 putHex (Number num, OutputStream && output_stream)
 {
-	output_stream . print (Util::toHex (std::forward <Number> (num)));
+	output_stream . print (Util::toHex (num));
 }
