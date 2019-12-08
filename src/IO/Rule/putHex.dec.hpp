694fabbf1f5c9071f14e3826c55c16098468cd4f
Zistack <Zistack@null.net>
2019-12-08 16:32:42 -0500

    Added missing std::enable_if

--- src/IO/Rule/putHex.dec.hpp
+++ src/IO/Rule/putHex.dec.hpp
@@ -1,3 +1,8 @@
-template <typename Number, typename OutputStream, typename>
+template
+<
+	typename Number,
+	typename OutputStream,
+	typename = std::enable_if_t <std::is_integral_v <Number>>
+>
 void
 putHex (Number num, OutputStream && output_stream);
