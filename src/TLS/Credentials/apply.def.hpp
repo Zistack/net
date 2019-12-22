7d3d3b0afb76fbc3babefdf3d8f459808499959d
Zistack <Zistack@null.net>
2019-12-22 13:40:30 -0500

    Removed unnecessary level of indiraction

--- src/TLS/Credentials/apply.def.hpp
+++ src/TLS/Credentials/apply.def.hpp
@@ -1,5 +1,20 @@
 void
 T::apply (gnutls_session_t session, const T & credentials)
 {
-	Util::setCredentials (session, credentials . m_credentials);
+	int err = gnutls_credentials_set
+	(
+		session,
+		GNUTLS_CRD_CERTIFICATE,
+		credentials . m_credentials
+	);
+	if (err != GNUTLS_E_SUCCESS)
+	{
+		throw Failure::Error::T
+		(
+			std::string ("Failed to set credentials for TLS session: ") +
+				gnutls_strerror (err) +
+				"\n"
+		);
+	}
 }
+
--- src/TLS/Util/setCredentials.dec.hpp
+++
@@ -1,6 +0,0 @@
-void
-setCredentials
-(
-	gnutls_session_t session,
-	const gnutls_certificate_credentials_t * credentials
-);
