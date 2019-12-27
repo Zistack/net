998fa64b35542a306162675f62b655e75c35b519
Zistack <Zistack@null.net>
2019-12-27 15:37:11 -0500

    Took advantage of new scope to remove redundant code

--- src/WebSocket/Message/Reader/operator_call.def.hpp
+++ src/WebSocket/Message/Reader/operator_call.def.hpp
@@ -3,24 +3,16 @@ template <typename InputStream>
 void
 T <OutputStream>::operator () (InputStream && input_stream)
 {
-	if constexpr (Failure::IsCancellable::T <InputStream>::value)
-	{
-		Scope::T entity_scope (this -> m_entity_slot, input_stream);
+	Failure::MaybeCancellableScope::T entity_scope
+	(
+		this -> m_entity_slot,
+		input_stream
+	);
 
-		Util::transfer
-		(
-			std::forward <InputStream> (input_stream),
-			this -> m_count,
-			std::forward <OutputStream> (this -> m_output_stream)
-		);
-	}
-	else
-	{
-		Util::transfer
-		(
-			std::forward <InputStream> (input_stream),
-			this -> m_count,
-			std::forward <OutputStream> (this -> m_output_stream)
-		);
-	}
+	Util::transfer
+	(
+		std::forward <InputStream> (input_stream),
+		this -> m_count,
+		std::forward <OutputStream> (this -> m_output_stream)
+	);
 }
--- src/WebSocket/Message/Writer/operator_call.def.hpp
+++ src/WebSocket/Message/Writer/operator_call.def.hpp
@@ -3,24 +3,16 @@ template <typename OutputStream>
 void
 T <InputStream>::operator () (OutputStream && output_stream)
 {
-	if constexpr (Failure::IsCancellable::T <OutputStream>::value)
-	{
-		Scope::T entity_scope (this -> m_entity_slot, output_stream);
+	Failure::MaybeCancellableScope::T entity_scope
+	(
+		this -> m_entity_slot,
+		output_stream
+	);
 
-		Util::transfer
-		(
-			std::forward <InputStream> (this -> m_input_stream),
-			this -> m_count,
-			std::forward <OutputStream> (output_stream)
-		);
-	}
-	else
-	{
-		Util::transfer
-		(
-			std::forward <InputStream> (this -> m_input_stream),
-			this -> m_count,
-			std::forward <OutputStream> (output_stream)
-		);
-	}
+	Util::transfer
+	(
+		std::forward <InputStream> (this -> m_input_stream),
+		this -> m_count,
+		std::forward <OutputStream> (output_stream)
+	);
 }
