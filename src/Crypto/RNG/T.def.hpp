b633c553ba02a504988fe69a90803845e7185f57
Zistack <Zistack@null.net>
2019-12-27 12:37:23 -0500

    Fixed RNG not seeding itself when the seed size is 0

--- src/Crypto/RNG/T.def.hpp
+++ src/Crypto/RNG/T.def.hpp
@@ -3,11 +3,10 @@ T::T (SeedGenerator && getSeed) : m_handle (newHandle ())
 {
 	uint32_t seed_size = kcapi_rng_seedsize (this -> m_handle . get ());
 
-	// Some RNGs don't have a seed size because they seed themselves.
-	if (! seed_size) return;
-
 	uint8_t seed_buf [seed_size];
-	getSeed (seed_buf, seed_size);
+
+	// Some RNGs don't have a seed size because they seed themselves.
+	if (seed_size) getSeed (seed_buf, seed_size);
 
 	if (kcapi_rng_seed (this -> m_handle . get (), seed_buf, seed_size))
 	{
